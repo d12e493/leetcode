f604d5a62af7a0d9083815e2ec5824cfcd6f53e8
davis <davis@10.10.29.153>
2016-06-07 18:22:19 +0800

    Excel換算

---
+++ src/easy/ExcelSheetColumnNumber_171.java
@@ -0,0 +1,26 @@
+package easy;
+
+public class ExcelSheetColumnNumber_171 {
+
+	public static int titleToNumber(String s) {
+
+		final int BASE_NUMBER = 26;
+
+		int result = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			int currentNumber = Character.getNumericValue(s.charAt(i)) - 9;
+//			System.out.println("currentNumber : " + currentNumber);
+			result += currentNumber * Math.pow(BASE_NUMBER, s.length() - 1 - i);
+//			System.out.println("result : " + result);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(titleToNumber("AB"));
+		System.out.println(titleToNumber("BA"));
+		System.out.println(titleToNumber("ABA"));
+	}
+
+}
