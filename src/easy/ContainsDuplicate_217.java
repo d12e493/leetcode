0fcbb7f75e38d94ebb6817648f5d16b5dd090ff8
davis <davis@10.10.29.153>
2016-06-08 16:30:10 +0800

    重複數字判斷

---
+++ src/easy/ContainsDuplicate_217.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate_217 {
+
+	public static boolean containsDuplicate(int[] nums) {
+
+		if (nums == null || nums.length == 0) {
+			return false;
+		}
+
+		Set<Integer> integerSet = new HashSet<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int current = nums[i];
+			if (integerSet.contains(current)) {
+				return true;
+			} else {
+				integerSet.add(current);
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[] test = new int[] { 11, 22, 33, 11 };
+		System.out.println(Arrays.toString(test) + " : " + containsDuplicate(test));
+
+		int[] test1 = new int[] { 11, 22, 33 };
+		System.out.println(Arrays.toString(test1) + " : " + containsDuplicate(test1));
+
+	}
+
+}
